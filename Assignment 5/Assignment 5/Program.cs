ee9c5f4575fa4ed9c66b61447e6f11ed8010c6f2
ridenour719 <ridenour719@gmail.com>
2022-03-27 10:29:27 -0400

    v0 Instructions

--- Assignment 5/Assignment 5/Program.cs
+++ Assignment 5/Assignment 5/Program.cs
@@ -10,6 +10,36 @@ namespace Assignment_5
     {
         static void Main(string[] args)
         {
+            /*
+             * Assignment 5 insturctions
+             * Loops and Arrays
+                • Declare one string variable teamName.
+                • Declare one int variable teamSize.
+                • Declare one string array variable playerNames.
+                • Declare one int array variable playerNumbers.
+
+            Write a void method called getTeamInfo that does the following:
+                • Ask the user to input the name of their team and assign that value to teamName.
+                • Ask the user to input the size of their team and assign that value to teamSize.
+                • Allocate enough memory for the playerNames array based on teamSize.
+                • Allocate enough memory for the playerNumbers array based on teamSize.
+                • Write a for loop that declares an int variable i to iterate teamSize times.
+                • Within that loop, at each iteration:
+                    o Ask the user for the current (i th) player’s name.
+                    o Store the input value in the current (i th) position in the playerNames array.
+                    o Ask the user for the current (i th) player’s number.
+                    o Store the input value in the current (i th) position in the playerNumbers array.
+            Write a void method called printTeamInfo that does the following:
+                • Print the teamName to the screen.
+                • Write a for loop that declares an int variable i to iterate teamSize times.
+                • Within that loop, at each iteration:
+                    o Print the the current (i th) player’s number to the screen.
+                    o Print the the current (i th) player’s name to the screen.
+                 Invoke getTeamInfo to create the team based on the user input.
+                 Invoke printTeamInfo to display the full team information on the screen.
+            */
+
+
         }
     }
 }
