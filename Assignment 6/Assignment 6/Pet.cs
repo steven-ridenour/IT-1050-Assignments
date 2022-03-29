8763ebcb9cbc7320bec97065c5acc49765b0b9ba
STEVEN RIDENOUR <ridenour719@gmail.com>
2022-03-29 14:22:38 -0400

    Assignment 6

---
+++ Assignment 6/Assignment 6/Pet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6
+{
+    internal class Pet
+    {
+        public int id;
+        public string name;
+        public string breed;
+        public int age;
+    }
+}
