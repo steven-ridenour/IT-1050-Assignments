485c6453388625ea50978125b1ffd2345ab63009
STEVEN RIDENOUR <ridenour719@gmail.com>
2022-03-31 10:17:21 -0400

    Assignment 5 v2

---
+++ Assignment 5v2/Assignment 5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+        Method method = new Method();
+            //updated Changes
+            method.getTeamInfo();
+            method.printTeamInfo();
+
+            Console.WriteLine();
+            Console.WriteLine("[PRESS ENTER TO CLOSE]");
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
