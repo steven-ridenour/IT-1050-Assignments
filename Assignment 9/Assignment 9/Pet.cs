d66da95fd4459b8bce1fed28951aaef349a3f3ed
ridenour719 <ridenour719@gmail.com>
2022-05-05 06:55:30 -0500

    Assignment 9

---
+++ Assignment 9/Assignment 9/Pet.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_9
+{
+    internal class Pet
+    {
+        
+        private string name;
+        private string breed;
+        private int age;
+        private static int totalNumberOfPets = 0;
+        private static int sumOfAllPetAges = 0;
+        private static int counter = 1;
+
+        public Pet ()
+        {
+
+            
+            Console.WriteLine("What is the pet {0}'s name? ", counter);
+            this.name = Console.ReadLine();
+            Console.WriteLine("");
+
+            Console.WriteLine("What is the pet {0}'s breed? ", counter);
+            this.breed = Console.ReadLine();
+            Console.WriteLine("");
+
+
+            Console.WriteLine("What is the pet {0}'s age? ", counter);
+            this.age = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            sumOfAllPetAges += age;
+            counter++;
+        }
+
+        public void getDetails()
+        {
+            Console.WriteLine(name + " is " + age + " years old, and is a " + breed + ".");
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public string getBreed()
+        {
+            return this.breed;
+        }
+
+        public int getAge()
+        {
+            return this.age;
+        }
+
+        public static int getTotalNumberOfPets()
+        {
+            return totalNumberOfPets;
+        }
+
+        public static int getSumOfAllPetAges()
+        {
+            return sumOfAllPetAges;
+        }
+
+        public static void setTotalNumberOfPets(int numOfPets)
+        {
+            totalNumberOfPets = numOfPets;
+        }
+
+       
+        
+    }
+}
---
+++ Assignment 9/Assignment 9/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Asks how many pets are owned
+            Console.WriteLine("How many pets do you own?");
+            Pet.setTotalNumberOfPets(int.Parse(Console.ReadLine()));
+            Console.Clear();
+
+
+            // Creates an Array of pets of size according to previous question
+            Pet[] pets = new Pet[Pet.getTotalNumberOfPets()];
+
+            for (int i = 0; i < Pet.getTotalNumberOfPets(); i++)
+            {
+                pets[i] = new Pet();
+            }
+
+            for (int i = 0; i < Pet.getTotalNumberOfPets(); i++)
+            {
+                pets[i].getDetails();
+
+            }
+
+            Pet youngest = pets[1];
+            Pet oldest = pets[1];
+
+            for (int i = 0; i < Pet.getTotalNumberOfPets(); i++)
+            {
+                if (pets[i].getAge() > youngest.getAge())
+                {
+                    oldest = pets[i];
+
+                }
+                if (pets[i].getAge() < youngest.getAge())
+                {
+                    youngest = pets[i];
+
+                }
+            }
+
+            double averageAge = (double)Pet.getSumOfAllPetAges() / (double)Pet.getTotalNumberOfPets();
+
+            Console.WriteLine("");
+            Console.WriteLine("The youngest pet is " + youngest.getName() + " at " + youngest.getAge() + " years old.");
+            Console.WriteLine("The oldest pet is " + oldest.getName() + " at " + oldest.getAge() + " years old.");
+            Console.WriteLine("");
+            Console.WriteLine("The average pet age is: " + averageAge);
+
+
+
+
+
+
+            Console.WriteLine("");
+            Console.WriteLine("[PRESS ENTER TO CLOSE]");
+            Console.ReadLine();
+        }
+    }
+}
